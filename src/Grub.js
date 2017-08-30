0d442b2a3c1aabc62f4d3829429ec779bff67cee
Hack Reactor Student <student@hackreactor.com>
2017-08-30 10:58:01 -0700

    save

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,12 @@
 class Grub {
+    //this.age = 0;
+    constructor () {
+        this.age = 0;
+        this.color = 'pink';
+        this.food = 'jelly';
+    }
+
+    eat () {
+        return 'Mmmmmmmmm jelly';
+    }
 };
